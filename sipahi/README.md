08148f5c2b2d1132483e3622523348db67227f15
safaanilatasoy <safaanilatasoy@gmail.com>
2024-09-04 15:59:00 +0300

    main

---
+++ sipahi/README.md
@@ -0,0 +1,2 @@
+# sipahi-apartment-administration
+ 
---
+++ sipahi/services.php
@@ -0,0 +1,128 @@
+﻿<!DOCTYPE html>
+<html lang="tr">
+<head>
+
+<!-- Basic Page Head -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Sipahi | Hizmetlerimiz</title>
+
+
+<!-- Mobile Meta -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+<!-- Favicons -->
+
+<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+
+<!-- Css -->
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="assets/css/jquery.bxslider.css">
+<link rel="stylesheet" type="text/css" href="assets/css/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="assets/css/owl.theme.css">
+<link rel="stylesheet" type="text/css" href="assets/css/prettyPhoto.css">
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+
+<!-- Google Fonts -->
+<link href="../../css?family=Dosis:200,300,400,500,600,700,800&amp;subset=latin,latin-ext" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+
+
+<!-- Site Back Top -->
+<div class="site-back-top" title="Back to top">
+	<i class="fa fa-angle-up"></i>
+</div>
+<!-- Site Back Top End -->
+
+<!-- Site Navigation -->
+<div class="site-nav"></div>
+<!-- Site Navigation End -->
+
+<!-- Site Container -->
+<div id="site-container" class="site-fullscreen site-sticky">
+	<!-- Header -->
+	<?php include_once("includes/header.php"); ?>
+	<!-- Header End -->
+	
+	<!-- Services -->
+	<section id="services">
+		<!-- Service Top -->
+		<div class="box-grey">
+			<div class="wrapper padding-all">
+				<!-- Section Header -->
+				<div class="section-header inline">
+					<h1>Hizmetlerimiz</h1>
+					<h3>Modern Yönetim, Çağdaş Yaşam.</h3>
+				</div>
+				<!-- Section Header End -->
+				
+				<div class="space"></div>
+				
+				<!-- Section Content -->
+				<div class="row">
+					<div class="col-md-6 col-sm-12">
+						<img src="assets/img/sipahilogo.jpg" alt="Services" class="img-full" style="width:65%;">
+					</div>
+					<div class="col-md-6 col-sm-12">
+						<h4 class="title-big">Profesyonel Site Yönetimi</h4>
+						<p>Sipahi Yönetim olarak, toplu yaşam alanlarınızın tüm ihtiyaçlarını profesyonel bir yaklaşımla karşılıyoruz. Temizlik hizmetlerinden bakım ve onarıma, bütçe yönetiminden peyzaj düzenlemeye kadar geniş bir hizmet yelpazesi sunuyoruz. Her bir hizmetimiz, konforunuzu ve güvenliğinizi ön planda tutarak, yaşam alanlarınızı en yüksek standartlarda tutmayı hedefliyoruz.</p>
+						<p>Alanında uzman ekibimiz, müşteri memnuniyetini öncelikli hedef olarak belirler ve tüm süreçlerde şeffaf ve etkili bir iletişim sağlar. Sizlere sunduğumuz kaliteli hizmetlerle, yaşam alanlarınızı daha düzenli, temiz ve konforlu hale getirmek için çalışıyoruz. Bizimle, her zaman güvende ve huzurlu bir ortamda yaşayabilirsiniz.</p>
+						
+						<div class="space"></div>
+						<div class="button-group clearfix">
+							<a href="about.php" class="btn btn-default">Hakkımızda</a>
+							<a href="contact.php" class="btn btn-dark">İletişim</a>
+						</div>
+					</div>
+				</div>
+				<!-- Section Content End -->
+			</div>
+		</div>
+		<!-- Service Top End -->
+		
+		<!-- Service List -->
+		<?php include("includes/serviceList.php"); ?>
+		<!-- Service List End -->
+		
+		<!-- Section Banner -->
+		<div class="box-dark-light">
+			<div class="wrapper padding-all">
+				<div class="section-header banner">
+					<h3>Bize duyduğunuz güven için minnettarız, bu güveni her zaman en üst düzeyde tutmayı hedefliyoruz.</h3>
+				</div>
+			</div>
+		</div>
+		<!-- Section Banner End -->
+	</section>
+	<!-- Services End -->
+		
+	<!-- Footer -->
+	<?php include("includes/footer.php"); ?>
+	<!-- Footer End -->
+</div>
+<!-- Site Container End -->
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/owl.carousel.js"></script>
+<script src="assets/js/jquery.bxslider.min.js"></script>
+<script src="assets/js/jquery.matchHeight.js"></script>
+<script src="assets/js/jquery.prettyPhoto.js"></script>
+<script src="assets/js/jquery.countTo.js"></script>
+<script src="assets/js/jquery.stellar.js"></script>
+<script src="assets/js/jquery.fitvids.js"></script>
+<script src="assets/js/imagesloaded.pkgd.js"></script>
+<script src="assets/js/masonry.pkgd.js"></script>
+
+<!-- Map Scripts -->
+<script src="https://maps.google.com/maps/api/js?sensor=false&language=en"></script>
+<script src="assets/js/gmap3.min.js"></script>
+
+<!-- Custom Scripts -->
+<script src="assets/js/scripts.js"></script>
+</body>
+</html>
